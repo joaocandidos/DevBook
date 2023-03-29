ac025dca68ccff79f905d1820f47725674cf91cb
joaocandidos <120682757+joaocandidos@users.noreply.github.com>
2023-03-29 15:30:09 -0300

    add API/src/repositorios/usuario.go

--- API/src/controllers/usuarios.go
+++ API/src/controllers/usuarios.go
@@ -1,9 +1,38 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/model"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("criando usuario"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	var usuario model.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
+
 }
 func BuscarTodosUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("buscando todos os usuario"))
